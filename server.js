b3c5a481203c81a14fcb6017f977492e2866fffa
lucas-oliveira17 <lucas_oliveira_viana@hotmail.com>
2020-10-05 20:48:02 -0300

    fix path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/conversion-app'));
+app.use(express.static('../../dist/conversion-app'));
 
 app.get('/*', function(req, res) {
     res.sendFile('index.html', {root: 'dist/conversion-app/'}
